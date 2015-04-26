c7dd65ae226515d715c5fb927ffc8bd698f61f5c
Joe Ren <earthengine@gmail.com>
2015-04-26 22:30:32 +1000

    Scrolling background!

--- Heros-Run-2D/Assets/Scripts/BoardManager.cs
+++ Heros-Run-2D/Assets/Scripts/BoardManager.cs
@@ -1,22 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour {
 
 	private Transform boardHolder;
 
+	public float scrollSpeed = 0.1f;
 	public GameObject backgroundObject;
+
+	private GameObject[] backgroundTiles;
 
 	private void BoardSetup(){
 		boardHolder = new GameObject ("Board").transform;
 
-		GameObject instance = 
-			Instantiate(backgroundObject, new Vector3(0,0,0f), 
-			            Quaternion.identity) as GameObject;
-		instance.transform.SetParent(boardHolder);
+		backgroundTiles = new GameObject[20];
+
+		for (var i=0; i<20; ++i) {
+			CreateBackground (i, new Vector3 ((i-10)*4.25f, 0, 0f));
+		}
+
 	}
 
 	public void SetupScene(){
 		BoardSetup ();
+	}
+
+	private void CreateBackground(int i, Vector3 v){
+		var instance = 
+			Instantiate(backgroundObject, v, 
+			            Quaternion.identity) as GameObject;
+		instance.transform.SetParent(boardHolder);
+		backgroundTiles [i] = instance;
 	}
 }
