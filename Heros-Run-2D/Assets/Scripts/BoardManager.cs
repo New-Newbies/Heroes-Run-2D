bf64f91bd566addd9ba6afb9ee503929ad5f06e3
Joe Ren <earthengine@gmail.com>
2015-04-26 18:03:52 +1000

    Showing the tile as background

--- Heros-Run-2D/Assets/Scripts/BoardManager.cs
+++ Heros-Run-2D/Assets/Scripts/BoardManager.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class BoardManager : MonoBehaviour {
 
-	private void BoardSetup(){
+	private Transform boardHolder;
 
+	public GameObject backgroundObject;
+
+	private void BoardSetup(){
+		boardHolder = new GameObject ("Board").transform;
+
+		GameObject instance = 
+			Instantiate(backgroundObject, new Vector3(0,0,0f), 
+			            Quaternion.identity) as GameObject;
+		instance.transform.SetParent(boardHolder);
 	}
 
 	public void SetupScene(){
